59265d4e10614f46e917d33db0eccd284dfdac79
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-07-19 01:14:11 +0200

    Working on getting plane details

---
+++ models/airplaneModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const AirplaneModelSchema = new Schema({
+  name: { type: String, required: true, maxLength: 100 },
+  vehicle_img: { type: String },
+  dynamic_url: { type: String },
+  turn_time_RB_stock: { type: Number },
+  turn_time_RB_upgraded: { type: Number },
+  max_speed_RB_stock: { type: Number },
+  max_speed_RB_upgraded: { type: Number },
+});
+
+// virtual for url
+
+AirplaneModelSchema.virtual("url").get(function () {
+  return `/${this.id}`;
+});
+
+module.exports = mongoose.model("Airplane", AirplaneModelSchema);
