1ad1a4b2e7c51a2b295cc377eebb0bf30fd027b2
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-07-21 22:45:06 +0200

    Working on fixing some of the code

--- controllers/vehicleController.js
+++ controllers/vehicleController.js
@@ -2,14 +2,6 @@ const Airplanes = require("../models/airplaneModel");
 const asyncHandler = require("express-async-handler");
 const { body, validationResult } = require("express-validator");
 const stat_helper = require("../lib/vehicle_stat_helper");
-// const bodyParser = require("body-parser");
-// const cheerio = require("cheerio");
-// const axios = require("axios");
-
-// helper function
-
-// function for getting specific vehicle array listing.
-// example of target URL = Germany_aircraft
 
 // get all vehicle list
 exports.all_vehicles = asyncHandler(async (req, res, next) => {
@@ -26,7 +18,9 @@ exports.all_vehicles = asyncHandler(async (req, res, next) => {
 
 exports.vehicle_compare_get = asyncHandler(async (req, res, next) => {
   // const airplanes = [{ name: "Yak-3" }, { name: "BF-109" }];
-  const airplanes = await Airplanes.find({}, "name rating_RB", {}).exec();
+  const airplanes = await Airplanes.find({}, "name rating_RB", {})
+    .sort({ name: 1 })
+    .exec();
   console.log(airplanes);
   res.render("vehicle_compare", {
     title: "Compare planes",
@@ -47,51 +41,10 @@ exports.vehicle_compare_post = [
     const plane = stat_helper.parsePOSTstring(req.body.plane);
     // convert battle ratings, return as object
     const ratingRangeObj = stat_helper.convertRating(plane.rating, req.body.br);
-    let items;
-
-    if (req.body.radioChoice == "speedDesc") {
-      console.log(req.body.radioChoice);
-      items = await Airplanes.find(
-        {
-          rating_RB: { $gte: ratingRangeObj.min, $lte: ratingRangeObj.max },
-        },
-
-        {}
-      )
-        .sort({ max_speed_RB: -1 })
-        .exec();
-    } else if (req.body.radioChoice == "turnAsc") {
-      console.log(req.body.radioChoice);
-      items = await Airplanes.find(
-        {
-          rating_RB: { $gte: ratingRangeObj.min, $lte: ratingRangeObj.max },
-        },
-
-        {}
-      )
-        .sort({ turn_time_RB: 1 })
-        .exec();
-    } else if (req.body.radioChoice == "climbDesc") {
-      console.log(req.body.radioChoice);
-      items = await Airplanes.find(
-        {
-          rating_RB: { $gte: ratingRangeObj.min, $lte: ratingRangeObj.max },
-        },
-
-        {}
-      )
-        .sort({ max_climb_RB: -1 })
-        .exec();
-    } else {
-      console.log(req.body.radioChoice);
-      items = await Airplanes.find(
-        {
-          rating_RB: { $gte: ratingRangeObj.min, $lte: ratingRangeObj.max },
-        },
-
-        {}
-      ).exec();
-    }
+    const items = await stat_helper.getDataFromInput(
+      req.body.radioChoice,
+      ratingRangeObj
+    );
 
     if (!errors.isEmpty()) {
       // errors found re-render
