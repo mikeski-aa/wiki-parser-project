e51c8c7d851d35b6a285e98979bb67595f1393cb
mikeski-aa <159564491+mikeski-aa@users.noreply.github.com>
2024-07-23 02:55:38 +0200

    back to square 1

--- scripts/vehiclesearch.js
+++ scripts/vehiclesearch.js
@@ -1,7 +1,7 @@
-function test() {
-  console.log("working!");
-}
-
-exports = {
-  test,
-};
+// script.js
+document.addEventListener("DOMContentLoaded", () => {
+  const button = document.getElementById("myButton");
+  button.addEventListener("click", () => {
+    console.log("Button clicked!");
+  });
+});
